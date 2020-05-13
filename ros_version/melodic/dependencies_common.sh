13e6bd5f5cbc361e8ebab71d5978c781d0382f2a
Joe Lin <kjoelovelife@gmail.com>
2020-05-13 16:30:36 +0800

    modified ros_version/melodic/dependencies_common.sh

--- ros_version/melodic/dependencies_common.sh
+++ ros_version/melodic/dependencies_common.sh
@@ -37,42 +37,43 @@ sudo apt install -y \
     ros-melodic-navigation \
     ros-melodic-joy \
     i2c-tools \
-    mosquitto-clients \
     python-pip \
-    python3-pip
+    python3-pip \
+    mosquitto-clients
 
-sudo python -m pip install --upgrade -pip
 
-sudo apt remove -y \
-	python-ruamel.yaml \
-	python-ruamel.ordereddict
+#sudo python -m pip install --upgrade -pip
+
+#sudo apt remove -y \
+#	python-ruamel.yaml \
+#	python-ruamel.ordereddict
 
 #sudo chgrp i2c /dev/i2c-1
 #sudo chmod 666 /dev/i2c-1
-sudo usermod -G i2c $USER
+sudo usermod -aG i2c $USER
 
 # set ominibot
 sudo cp ominibot.rules /etc/udev/rules.d
 
 
 # download vision_opencv
-git clone https://github.com/ros-perception/vision_opencv ~/Knight_car/catkin_ws/src/vision_opencv
+#git clone https://github.com/ros-perception/vision_opencv ~/Knight_car/catkin_ws/src/vision_opencv
 
 # configure ydlidar
-sudo sh ~/Knight_car/catkin_ws/src/ydlidar/startup/initrnv.sh
+sudo sh ~/Knight_car/catkin_ws/src/ydlidar/startup/initenv.sh
 
-sudo udevadm control --reload-rules
-sudo udevadm trigger
+#sudo udevadm control --reload-rules
+#sudo udevadm trigger
 
 # These don't have an APT package
 
-pip install --upgrade --user \
-	PyContracts==1.7.15 \
-        DecentLogs==1.1.2 \
-	conftools==1.9.1 \
-	procgraph==1.10.6 \
-	pymongo==3.5.1 \
-	ruamel.yaml==0.15.34
+#pip install --upgrade --user \
+#	PyContracts==1.7.15 \
+#        DecentLogs==1.1.2 \
+#	conftools==1.9.1 \
+#	procgraph==1.10.6 \
+#	pymongo==3.5.1 \
+#	ruamel.yaml==0.15.34
 
 # pip havs problem to install
 # QuickApp==1.3.8 comptests==1.4.10

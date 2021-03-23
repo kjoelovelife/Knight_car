b8c4c5a6a36093a42bd1ac6ee12dee1bfb6401a6
Joe Lin <kjoelovelife@gmail.com>
2021-03-23 15:13:39 +0800

    add ros version: noetic

--- ros_version/noetic/ros_noetic_install.sh
+++ ros_version/noetic/ros_noetic_install.sh
@@ -31,7 +31,7 @@ if [[ `id -u` -eq 0 ]] ; then
     exit 1 ;
 fi
 
-
+sudo apt install python
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
@@ -41,8 +41,8 @@ sudo apt-get install -y ros-noetic-desktop-full
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
-sudo apt-get install -y python-rosinstall python-rosinstall-generator python-wstool build-essential
-sudo apt install -y python-rosdep
+sudo apt-get install -y python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install -y python3-rosdep
 sudo rosdep init
 rosdep update
 
